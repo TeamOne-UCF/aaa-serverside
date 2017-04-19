9049b38680cdd325322c01cb93b628e8f227d448
Harold <username@users.noreply.github.com>
2017-04-19 00:41:07 -0400

    Fix the update customer function

--- dataAccess/customerRepository.js
+++ dataAccess/customerRepository.js
@@ -99,9 +99,17 @@ function insertCustomer(req, res, next) {
     var newCustomer = req.body;
     dbConnection.executeQuery(
         "CALL Customer_Reception_Database.InsertCustomer(?,?,?,?,?,?,?,?,?,?,?)",
-        [newCustomer.p_first_name, newCustomer.p_last_name, newCustomer.p_email, newCustomer.p_phone, newCustomer.p_address,
-            newCustomer.p_address_2, newCustomer.p_city, newCustomer.p_state, newCustomer.p_postal_code,
-             newCustomer.p_alert_email, newCustomer.p_alert_sms
+        [newCustomer.p_first_name,
+            newCustomer.p_last_name,
+            newCustomer.p_email,
+            newCustomer.p_phone,
+            newCustomer.p_address,
+            newCustomer.p_address_2,
+            newCustomer.p_city,
+            newCustomer.p_state,
+            newCustomer.p_postal_code,
+            newCustomer.p_alert_email,
+            newCustomer.p_alert_sms
         ],
         function(err, results) {
             if(err) {
@@ -115,19 +123,20 @@ function insertCustomer(req, res, next) {
 function updateCustomer(req, res, next) {
 		var updatedCustomer = req.body;
 		
-		dbConnection.executeQuery("UPDATE ?? SET first_name = ?, last_name = ?, email = ?, phone = ?, address = ?, city = ?, state = ?, postal_code = ?, alert_email = ?, alert_sms = ? WHERE customer_id = ?;",
-			["Customer_Reception_Database.Customer",
-				updatedCustomer.p_first_name, 
+		dbConnection.executeQuery(
+            "CALL Customer_Reception_Database.UpdateCustomer(?,?,?,?,?,?,?,?,?,?,?,?)",
+			[   updatedCustomer.p_customer_id,
+                updatedCustomer.p_first_name,
 				updatedCustomer.p_last_name, 
-				updatedCustomer.p_email, 
-				updatedCustomer.p_phone, 
-				updatedCustomer.p_address,
-				updatedCustomer.p_city, 
-				updatedCustomer.p_state, 
-				updatedCustomer.p_postal_code,
-				updatedCustomer.p_alert_email, 
-				updatedCustomer.p_alert_sms,
-				updateCustomer.p_customer_id, 
+				updatedCustomer.p_email,
+                updatedCustomer.p_phone,
+                updatedCustomer.p_address,
+                "",
+                updatedCustomer.p_city,
+                updatedCustomer.p_state,
+                updatedCustomer.p_postal_code,
+                updatedCustomer.p_alert_email,
+                updatedCustomer.p_alert_sms,
 			],
 			function(err, results) {
 				if(err)
@@ -169,7 +178,6 @@ function insertCustomerToQueue(req, res, next) {
             }
         });
 }
-
 
 function sendCustomerSms(firstName, phone, serviceId) {
 	var smsBody = 'Hello ' + firstName + '\n\nWe will notify you when an agent is ready. ' +
